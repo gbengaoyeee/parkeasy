ec8dff597a32d8b01edd9f7fd03e705de1d4eaf7
gbengaoyeee <24610110+gbengaoyeee@users.noreply.github.com>
2024-01-12 02:22:28 -0500

    Update user.ts

--- management-app/src/api/user.ts
+++ management-app/src/api/user.ts
@@ -4,6 +4,5 @@ let client = new ApiClient('user').client
 
 export const getUser = async (email: string) => {
     const response = await client.get(`/email/${email}`)
-    console.log(response.data)
     return response.data.data
 }
