0cb03c67e1d4e27c92bc0fbf4fa78bcd30d7ac77
gbengaoyeee <24610110+gbengaoyeee@users.noreply.github.com>
2024-03-13 00:43:39 -0400

    Update webhooks.module.ts

--- backend/src/webhooks/webhooks.module.ts
+++ backend/src/webhooks/webhooks.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { WebhooksController } from './webhooks.controller';
 import { WebhooksService } from './webhooks.service';
 import { StripeService } from 'src/stripe/stripe.service';
+import { QueuesModule } from 'src/queues/queues.module';
 
 @Module({
+  imports: [
+    QueuesModule.register(),
+  ],
   controllers: [WebhooksController],
   providers: [WebhooksService, StripeService],
 })
