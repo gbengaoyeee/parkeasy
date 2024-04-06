c3fb36d1b2fdcae2401afa1aa10a933255a20f2c
gbengaoyeee <24610110+gbengaoyeee@users.noreply.github.com>
2024-04-06 15:00:21 -0400

    Update VisitorViewSpot.tsx

--- management-app/src/_root/pages/VisitorViewSpot.tsx
+++ management-app/src/_root/pages/VisitorViewSpot.tsx
@@ -31,7 +31,7 @@ const VisitorViewSpot = () => {
     <div className="flex flex-col gap-5">
       <h3 className="base-semibold">Parking spot information</h3>
       <div className="flex justify-between text-gray-500 shadow-md rounded-lg p-4 border">
-        <div className="grid grid-cols-2 gap-10 small-regular">
+        <div className="lg:grid grid-cols-2 gap-10 small-regular">
           <div className="flex flex-col gap-2">
             <Label className="base-semibold">Parking spot number</Label>
             <p>{parkingSpot.parking_spot_number}</p>
